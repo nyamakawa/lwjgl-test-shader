57853612035eca37cc2b144140ad823c085cef4f
nyamakawa2 <nyamakawa2@gmail.com>
2014-08-17 12:39:16 +0900

    cleanup

--- src/lwjgl_test/core.clj
+++ src/lwjgl_test/core.clj
@@ -32,32 +32,32 @@
   (println log))
 
 (defn shader-error [shader]
-     (let [log-length (GL20/glGetShaderi shader GL20/GL_INFO_LOG_LENGTH)]
-      (let [error-log (GL20/glGetShaderInfoLog shader log-length)]
+     (let [log-length (GL20/glGetShaderi shader GL20/GL_INFO_LOG_LENGTH)
+           error-log (GL20/glGetShaderInfoLog shader log-length)]
         (put-error error-log)
         (GL20/glDeleteShader shader)
-        nil)))
+        nil))
 
 ;; shader-type: glEnum
 ;; shader-src:  shader string
 (defn load-shader [shader-type shader-src]
-  (def shader (GL20/glCreateShader shader-type))
+  (let [shader (GL20/glCreateShader shader-type)]
   ;; convert string to java.nio.ByteBuffer
   ;;  (def shader-bytes (util/prepare-string shader-src))
-  (println "shader: " shader)
-  (println "shader-src: " shader-src)
+    (do
+      (println "shader: " shader)
+      (println "shader-src: " shader-src)
 
-  (GL20/glShaderSource shader shader-src)
-  (GL20/glCompileShader shader)
+      (GL20/glShaderSource shader shader-src)
+      (GL20/glCompileShader shader)
 
   ;; check compile status
-  (def compiled?
-    (= GL11/GL_TRUE (GL20/glGetShaderi shader GL20/GL_COMPILE_STATUS)))
-  (println "compiled? :" compiled?)
-
-  (if compiled?
-    shader
-    (shader-error shader)))
+      (let [compiled? (= GL11/GL_TRUE (GL20/glGetShaderi shader GL20/GL_COMPILE_STATUS))]
+        (do
+          (println "compiled? :" compiled?)
+          (if compiled?
+            shader
+            (shader-error shader)))))))
 
 (defn check-error []
   (let [gl-error (GL11/glGetError)]
@@ -65,43 +65,46 @@
       (println "error: " (Util/translateGLErrorString gl-error))
     )))
 
+(defn vertices-bytebuffer []
+  (let  [vertices [0.0 0.5 0.0
+                   -0.5 -0.5 0.0
+                   0.5 -0.5 0.0]
+         vertices-buffer (BufferUtils/createFloatBuffer (count vertices))]
+    (do
+      (doto vertices-buffer
+        (.put (float-array vertices))
+        (.flip))
+      vertices-buffer)))
+
 (defn draw [program]
-  (def vertices [0.0 0.5 0.0
-                 -0.5 -0.5 0.0
-                 0.5 -0.5 0.0])
-  (def vertices-buffer (BufferUtils/createFloatBuffer (count vertices)))
-  (doto vertices-buffer
-     (.put (float-array vertices))
-     (.flip))
+  (let [vertices-buffer (vertices-bytebuffer)]
+    (do
+      (GL11/glViewport 0 0 width height)
+      (GL11/glClear GL11/GL_COLOR_BUFFER_BIT)
 
-  (GL11/glViewport 0 0 width height)
-  (GL11/glClear GL11/GL_COLOR_BUFFER_BIT)
-
-  (GL20/glUseProgram program)
-  ;;(GL20/glVertexAttribPointer 0 3 false 0 vertices-buffer)
-  (let [buf-id (GL15/glGenBuffers)]
-;;    (println "buf-id: " buf-id)
-    (check-error)
-    (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER buf-id)
-    (check-error)
-    (GL15/glBufferData GL15/GL_ARRAY_BUFFER vertices-buffer GL15/GL_STATIC_DRAW)
-    (check-error)
-    )
+      (GL20/glUseProgram program)
+      ;;(GL20/glVertexAttribPointer 0 3 false 0 vertices-buffer)
+      (let [buf-id (GL15/glGenBuffers)]
+        ;;    (println "buf-id: " buf-id)
+        (do
+          (check-error)
+          (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER buf-id)
+          (check-error)
+          (GL15/glBufferData GL15/GL_ARRAY_BUFFER vertices-buffer GL15/GL_STATIC_DRAW)
+          (check-error)))))
 
 ;;  (GL20/glEnableVertexAttribArray 0)
   (GL11/glEnableClientState GL11/GL_VERTEX_ARRAY)
   (GL11/glVertexPointer 3 GL11/GL_FLOAT 0 0)
-
-  (GL11/glDrawArrays GL11/GL_TRIANGLES 0 (/ (count vertices) 3))
-
+  (GL11/glDrawArrays GL11/GL_TRIANGLES 0 3)
   (Util/checkGLError))
 
 (defn shader-program-error [program]
-  (let [log-length (GL20/glGetProgrami program GL20/GL_INFO_LOG_LENGTH)]
-    (let [error-log (GL20/glGetProgramInfoLog program log-length)]
+  (let [log-length (GL20/glGetProgrami program GL20/GL_INFO_LOG_LENGTH)
+        error-log (GL20/glGetProgramInfoLog program log-length)]
       (put-error error-log)
       (GL20/glDeleteProgram program)
-      nil)))
+      nil))
 
 (defn draw-loop-with-program [program]
   (GL11/glClearColor 0.0 0.0 0.0 0.0)
@@ -126,28 +129,26 @@
         (GL20/glLinkProgram program)
 
         (let [linked? (= GL11/GL_TRUE (GL20/glGetProgrami program GL20/GL_LINK_STATUS))]
-
           (GL20/glBindAttribLocation program 0 "vPosition")
 
           (if linked?
-
             (draw-loop-with-program program)
             (shader-program-error program)
             ))))))
-
 
 (defn start []
   (Display/setDisplayMode display-mode)
   (Display/setTitle "Hello GL")
   (Display/create)
 
-  (def vertex-shader (load-shader GL20/GL_VERTEX_SHADER vertex-shader-src))
-  (def pixel-shader (load-shader GL20/GL_FRAGMENT_SHADER pixel-shader-src))
-  (println "vertex-shader: " vertex-shader)
-  (println "pixel-shader: " pixel-shader)
+  (let [vertex-shader (load-shader GL20/GL_VERTEX_SHADER vertex-shader-src)
+        pixel-shader (load-shader GL20/GL_FRAGMENT_SHADER pixel-shader-src)]
+      (do
+        (println "vertex-shader: " vertex-shader)
+        (println "pixel-shader: " pixel-shader)
 
-  (if (and vertex-shader pixel-shader)
-    (start-draw-loop vertex-shader pixel-shader)))
+        (if (and vertex-shader pixel-shader)
+          (start-draw-loop vertex-shader pixel-shader)))))
 
 (defn -main [& args]
   (println "Starting...")
